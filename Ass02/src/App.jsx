10dde3a605f839b17886e306a65f0376d3cc7df9
Khuzaima <114830749+KhuzaimaIqbal123@users.noreply.github.com>
2024-12-08 02:44:03 +0500

    Component 3,4 rendered

--- Ass02/src/App.jsx
+++ Ass02/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import C1 from "../Components/c1";
 import C2 from "../Components/c2";
+// import C3 from "../Components/c3";
+import C4 from "../Components/c4";
 
 const App = () =>{
   return (
    <div>
 	<C1 />
 	<C2 />
+  {/* <C3/> */}
+  <C4/>
 	</div>
 
     
