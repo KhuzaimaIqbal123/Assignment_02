416646b82ad4e7e48c792eceaf6526f790128af1
Khuzaima <114830749+KhuzaimaIqbal123@users.noreply.github.com>
2024-12-22 14:51:57 +0500

    CSS done now yummy page is ready

--- Ass02/src/main.jsx
+++ Ass02/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-//import "bootstrap/dist/css/bootstrap.min.css";
+import './assests/vendor/bootstrap/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')).render(
     <div>
